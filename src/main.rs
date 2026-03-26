d2bcb564075b779e00f24ec89c0fbbec25754886
ThomasLeMontagner <thomas.le-montagner@hotmail.fr>
2026-03-26 07:14:14 +0100

    implemented rudimentary telemetry simulator

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,30 @@ struct Telemetry {
 }
 
 impl Telemetry {
-    fn nominal() -> Self {
+
+    fn from_step(step: u32) -> Self {
         Self {
-            battery_voltage: 12.4,
-            temperature_c: 42.0,
-            cpu_load_percent: 37.5,
+            battery_voltage: 12.4 - (step as f32 * 0.1),
+            temperature_c: 42.0 + (step as f32 * 0.8),
+            cpu_load_percent: 37.5 + (step as f32 * 2.5),
         }
     }
 }
 
 fn main() {
-    loop {
-        let sample = Telemetry::nominal();
-        print_telemetry(&sample);
+    let mut step = 0;
 
+    loop {
+        let sample = Telemetry::from_step(step);
+        print_telemetry(step, &sample);
+
+        step += 1;
         thread::sleep(Duration::from_secs(1));
     }
 }
 
-fn print_telemetry(telemetry: &Telemetry) {
-    println!("Satellite telemetry sample");
+fn print_telemetry(step: u32, telemetry: &Telemetry) {
+    println!("Satellite telemetry sample {step}");
     println!("Battery voltage: {:.1} V", telemetry.battery_voltage);
     println!("Temperature: {:.1} °C", telemetry.temperature_c);
     println!("CPU load: {:.1} %", telemetry.cpu_load_percent);
